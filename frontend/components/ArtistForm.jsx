481bfce1c641eecea3984d1003173bb93f25804d
Kevin Dolch <dolchk@gmail.com>
2016-03-21 13:40:01 -0700

    fixed redirection bug

--- frontend/components/ArtistForm.jsx
+++ frontend/components/ArtistForm.jsx
@@ -3,7 +3,8 @@ var ApiUtil = require('../util/api_util');
 var LinkedStateMixin = require('react-addons-linked-state-mixin');
 var hashHistory = require('react-router').hashHistory;
 var ArtistStore = require('../stores/artist');
-var ErrorStore = require('../stores/error')
+var ErrorStore = require('../stores/error');
+var SongStore = require('../stores/song');
 
 var ArtistForm = React.createClass({
   mixins: [LinkedStateMixin],
@@ -12,14 +13,29 @@ var ArtistForm = React.createClass({
     return {
       name: "",
       description: "",
-      errors: []
+      errors: [],
+      songs: SongStore.all()
     };
   },
 
   componentDidMount: function() {
     ErrorStore.clear();
     this.errorToken = ErrorStore.addListener(this._onError);
+    this.songToken = SongStore.addListener(this._onChange);
+    ApiUtil.fetchSomeSongs({flag: "For Splash"});
+    if(this.state.songs) {
+      if (!this.state.songs[0][0].logged_in) {
+        window.location.href= "/session/new";
+      }
+    }
 
+  },
+
+  _onChange: function() {
+    this.setState({ songs: SongStore.all()});
+    if (!this.state.songs[0][0].logged_in) {
+      window.location.href= "/session/new";
+    }
   },
 
   _onError: function() {
--- frontend/components/SongForm.jsx
+++ frontend/components/SongForm.jsx
@@ -44,6 +44,7 @@ var SongForm = React.createClass({
 
   componentWillUnmount: function() {
     this.errorToken.remove();
+    this.songToken.remove();
   },
 
   handleSubmit: function(e){
